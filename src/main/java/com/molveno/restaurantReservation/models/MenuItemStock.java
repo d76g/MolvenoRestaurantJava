7fc4295efde1b73d4c31e1941637a0ed32b8eac6
eboalii <albeshari_96@hotmail.com>
2024-07-15 15:21:25 +0200

    hello bashar

--- src/main/java/com/molveno/restaurantReservation/models/MenuItemStock.java
+++ src/main/java/com/molveno/restaurantReservation/models/MenuItemStock.java
@@ -23,47 +23,12 @@ public class MenuItemStock {
     @JoinColumn(name = "kitchenStock_id")
     private KitchenStock kitchenStock;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public double getAmount() {
         return amount;
     }
 
     public void setAmount(double amount) {
         this.amount = amount;
-    }
-
-    public Menu getMenuItem() {
-        return menu;
-    }
-
-    public void setMenuItem(Menu menu) {
-        this.menu = menu;
-    }
-
-    public KitchenStock getKitchenStock() {
-        return kitchenStock;
-    }
-
-    public void setKitchenStock(KitchenStock kitchenStock) {
-        this.kitchenStock = kitchenStock;
-    }
-
-    public Menu getMenu() {
-        return menu;
-    }
-
-    public void setMenu(Menu menu) {
-        this.menu = menu;
-    }
-
-    public MenuItemStock() {
     }
 
     public long getId() {
@@ -88,13 +53,5 @@ public class MenuItemStock {
 
     public void setKitchenStock(KitchenStock kitchenStock) {
         this.kitchenStock = kitchenStock;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
     }
 }
