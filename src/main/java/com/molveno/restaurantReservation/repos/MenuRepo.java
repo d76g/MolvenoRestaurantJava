2e4f9b067e8db2653f1ff8315b919a82cc4bb0c1
haltaynl <hatice.altay1@icloud.com>
2024-07-22 13:33:27 +0200

    Menu images

--- src/main/java/com/molveno/restaurantReservation/repos/MenuRepo.java
+++ src/main/java/com/molveno/restaurantReservation/repos/MenuRepo.java
@@ -1,9 +1,17 @@
 package com.molveno.restaurantReservation.repos;
 
+import com.molveno.restaurantReservation.models.DTO.MenuDTO;
+import com.molveno.restaurantReservation.models.MealTime;
 import com.molveno.restaurantReservation.models.Menu;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MenuRepo extends CrudRepository<Menu, Long > {
+    @Query("SELECT m FROM Menu m WHERE m.mealTime.mealTime_name = :mealTimeName")
+    List<Menu> findByMealTimeName(@Param("mealTimeName") String mealTimeName);
 }
